ccf20f95c1a14b701f43485da8eccefb823e07d3
SanaPasado <cscunanan@student.hau.edu.ph>
2026-05-07 09:37:58 +0800

    working path

--- axilearn/app/gemini.js
+++ axilearn/app/gemini.js
@@ -1,7 +1,13 @@
+import dotenv from 'dotenv';
 import {GoogleGenAI} from '@google/genai';
+import {dirname, resolve} from 'node:path';
+import {fileURLToPath} from 'node:url';
 
-const CANVAS_API_TOKEN = process.env.CANVAS_API_TOKEN;
-console.log('CANVAS_API_TOKEN:', CANVAS_API_TOKEN);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: resolve(__dirname, '../.env.local') });
+
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 console.log('GEMINI_API_KEY:', GEMINI_API_KEY);
 const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
