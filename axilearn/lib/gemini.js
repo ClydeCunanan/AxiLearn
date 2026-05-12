ca2660dd98ec8e2c3470b883a6975a093b6ec59d
SanaPasado <cscunanan@student.hau.edu.ph>
2026-05-12 08:59:14 +0800

    working prisma

--- axilearn/lib/gemini.js
+++ axilearn/lib/gemini.js
@@ -11,11 +11,15 @@ const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
  export default async function main(message) {
   const chat = ai.chats.create({
     model: "gemini-3-flash-preview",
-    history: []
+    history: [],
+    config: {
+      systemInstructions: "You're the AxiLearn AI assistant, designed to help students with their learning needs. You can provide explanations, answer questions, and offer guidance on a wide range of topics. Always strive to be clear, concise, and helpful in your responses. Be consice and technical in your answers. If you don't know the answer, say you don't know. Always ask for clarification if the user's question is ambiguous.",}
   });
+
   const response = await chat.sendMessage({
     message: message
   });
+  
   
 chat.history.push({ role: "user", parts: [{ text: message }] }); 
 chat.history.push({ role: "model", parts: [{ text: response.text }] }); 
---
+++ axilearn/prisma.config.ts
@@ -0,0 +1,15 @@
+'use server'
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+   
+  },
+}
+);
